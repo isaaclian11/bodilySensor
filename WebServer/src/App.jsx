d70bd3cdef21f0292fbdab9490eb1ad5ae902988
Justin <jmworley@iastate.edu>
2020-09-15 21:05:54 -0500

    Added Firebase signup as main page

--- WebServer/src/App.jsx
+++ WebServer/src/App.jsx
@@ -2,8 +2,11 @@ import React from 'react';
 import { BrowserRouter as Router, Route } from 'react-router-dom';
 import Home from './components/Home';
 import About from './components/About';
+import SignUpForm from './components/SignUp';
+
 import './App.css';
 import Navbar from './components/CustomNavbar';
+
 
 
 function App() {
@@ -11,8 +14,9 @@ function App() {
       <Router>
         <div>
           <Navbar />
-          <Route exact path="/" component={Home} />
+          <Route exact path="/" component={SignUpForm} />
           <Route path="/about" component={About} />
+          <Route path="/home" component={Home} />
         </div>
       </Router>
   );
