34aefaea39aacbab76acebf7e54cbc34262d6f9e
Justin <jmworley@iastate.edu>
2020-11-01 19:46:08 -0600

    adding website app to branch

---
+++ WebServer/build/precache-manifest.8d44603a376ef42bdd37d5a149b2c967.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4910a4e07a5684339d9859f9e3bd60ea",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c78e39415d4e24e77586",
+    "url": "/static/css/main.e172a677.chunk.css"
+  },
+  {
+    "revision": "046a2e8a83af0f4ff089",
+    "url": "/static/js/2.5df00a9b.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/static/js/2.5df00a9b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c78e39415d4e24e77586",
+    "url": "/static/js/main.1e57c017.chunk.js"
+  },
+  {
+    "revision": "1ceac58eaca1d82d7a90",
+    "url": "/static/js/runtime-main.6a11d49d.js"
+  }
+]);
